062bc204432a7a7c6df6017c0712da8d9bece048
Diaa Mahfouz <diaamahfouz229@gmail.com>
2024-09-15 20:34:46 +0300

    implement watchList ui

--- lib/browse/view/widgets/Browse.dart
+++ lib/browse/view/widgets/Browse.dart
@@ -6,32 +6,30 @@ class Browse extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.1,
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      SizedBox(
+        height: MediaQuery.of(context).size.height * 0.1,
+      ),
+      Padding(
+        padding: const EdgeInsets.only(left: 20.0),
+        child: Text(
+          'Browse Category',
+          style: Theme.of(context).textTheme.titleLarge,
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20.0),
-          child: Text(
-            'Browse Category',
-            style: Theme.of(context).textTheme.titleLarge,
+      ),
+      Expanded(
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 24,
+            childAspectRatio: 2,
           ),
+          itemCount: 12,
+          itemBuilder: (_, int index) {
+            return const CategoryItem();
+          },
         ),
-        Expanded(
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 24,
-              childAspectRatio: 2,
-            ),
-            itemCount: 12,
-            itemBuilder: (_, int index) {
-              return const CategoryItem();
-            },
-          ),
-        ),
-      ]),
-    );
+      ),
+    ]);
   }
 }
