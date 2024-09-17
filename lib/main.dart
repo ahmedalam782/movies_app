1135b8a4b7cb6066fe9b11d33a505b29e43fb1d7
ahmedalam <ahmedalam4887482@gmail.com>
2024-09-17 07:30:14 +0300

    design homeDetails with and new components to use

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movies_app_route/home/view/screen/home_screen.dart';
 import 'package:movies_app_route/shared/themes/app_theme.dart';
 
-void main() {
-
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MovieApp());
 }
 
--- lib/shared/themes/app_theme.dart
+++ lib/shared/themes/app_theme.dart
@@ -4,11 +4,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static const Color primary = Color(0xffFFBB3B);
-  static const Color black = Color(0xff1E1E1E);
+  static const Color black = Color(0xff121312);
   static const Color gray = Color(0xffC6C6C6);
   static const Color white = Color(0xffffffff);
-  static const Color darkGray = Color(0xff514F4F);
-
+  static const Color darkGray = Color(0xff282A28);
   static ThemeData lightTheme = ThemeData(
       scaffoldBackgroundColor: black,
       appBarTheme: AppBarTheme(
